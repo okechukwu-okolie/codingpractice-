de6f40e5943878bcbded8c3b33897b5b64ecbd8b
Okolie Samuel <okechukwu.okolie.2023@gmail.com>
2025-08-19 13:53:09 +0100

    another commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 
 
 function App() {
- 
+ let searchSentence = 'this is the man he has a leg'
+ let result = searchSentence.match(/men/)
+ let result2 = searchSentence.replace(/man/,'woman')
+
+ if(result){
+  console.log(`the word man exists in the search sentence ${searchSentence}`)
+ }
+ if(result2){
+  console.log(`the text of man has been changed to woman in the search sentence ${result2}`)
+ }
+ else{
+  console.log(`the search word does `)
+ }
 
   return (
     <>
