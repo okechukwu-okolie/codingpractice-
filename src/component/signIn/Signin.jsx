ad1abc924dd7f6f6601e65133790a0f716d0ba31
Okolie Samuel <okechukwu.okolie.2023@gmail.com>
2025-08-21 06:55:13 +0100

    adding some more code to the scheduler

--- src/component/signIn/Signin.jsx
+++ src/component/signIn/Signin.jsx
@@ -1,10 +1,63 @@
-import React from 'react'
+
+import React,{useState} from 'react'
+import {Link} from 'react-router-dom'
+import { BsChevronDoubleLeft } from 'react-icons/bs'
+import './signin.css'
 
 const Signin = () => {
+
+   const [signinusername, setSigninUsername] = useState('')
+   const [signinpassword, setSigninPassword] = useState('')
+  
+  
+    const handleSubmit=(e)=>{
+      e.prevent.default()
+    }
   return (
-    <div class='signIn'>
-      this is the sign in page
-    </div>
+    <>
+          <Link to='/signUp'>
+            <div class='signup-back' >
+              <BsChevronDoubleLeft size={12} style={{fontWeight:'bolder'}}/>
+              <p>back</p>
+            </div>
+          </Link>
+          
+        <div class='signUp'>
+    
+    
+           <form onSubmit={handleSubmit} 
+           
+           class='signin-form'>
+            <div style={{display:'flex',alignItem:'center'}}><p>Sign In</p></div>
+          
+                  <input 
+                    type="text"
+                    value={signinusername}
+                    onChange={(e)=>setSigninUsername(e.target.value)}
+                    placeholder='Username'
+                    class='Username'
+                    />
+          
+                    <input 
+                    type="password"
+                    value={signinpassword}
+                    onChange={(e)=>setSigninPassword(e.target.value)}
+                    placeholder='Password'
+                    class='Password'
+                    />
+                    
+                    <button type='submit'> Submit</button>
+    
+                   <Link to='/signIn'>
+                      <div class='signin-signin'>
+                          <p>Click to sign in here</p>
+                      </div>
+                    </Link> 
+          </form>
+          
+          
+        </div>
+    </>
   )
 }
 
--- src/component/signUp/Signup.jsx
+++ src/component/signUp/Signup.jsx
@@ -1,70 +1,33 @@
 import React,{useState} from 'react'
-// import { Button, Modal } from "antd";
+import {Link} from 'react-router-dom'
+import { BsChevronDoubleLeft } from 'react-icons/bs'
 import './signUp.css'
-import { BiChevronLeft } from 'react-icons/bi'
 
 const Signup = () => {
-//  const [isModalOpen, setIsModalOpen] = useState(false);
+
  const [username, setUsername] = useState('')
  const [password, setPassword] = useState('')
 
-  // const showModal = () => {
-  //   setIsModalOpen(true);
-  // };
 
-  // const handleOk = () => {
-  //   setIsModalOpen(false);
-  // };
-
-  // const handleCancel = () => {
-  //   setIsModalOpen(false);
-  // };
   const handleSubmit=(e)=>{
     e.prevent.default()
   }
 
   return (
 
-    
+    <>
+      <Link to='/'>
+        <div class='signup-back' >
+          <BsChevronDoubleLeft size={12} style={{fontWeight:'bolder'}}/>
+          <p>back</p>
+        </div>
+      </Link>
     <div class='signUp'>
-      <BiChevronLeft/>
-    
-    {/* <Button type="primary" onClick={showModal}>
-        Open Modal
-      </Button>
-      <Modal
-      title="Basic Modal"
-        closable={{ 'aria-label': 'Custom Close Button' }}
-        open={isModalOpen}
-        onOk={handleOk}
-        onCancel={handleCancel}
-      >
-<form onSubmit={handleSubmit}>
-
-        <input 
-          type="text"
-          value={username}
-          onChange={(e)=>setUsername(e.target.value)}
-          placeholder='Username'
-          class='Username'
-          />
-
-          <input 
-          type="text"
-          value={password}
-          onChange={(e)=>setPassword(e.target.value)}
-          placeholder='Password'
-          class='Password'
-          />
-          <br />
-          <button type='submit'> Submit</button>
-          </form>
-          
-          </Modal> */}
 
 
        <form onSubmit={handleSubmit} 
        class='signup-form'>
+            <div style={{display:'flex',alignItem:'center'}}><p>Sign Up</p></div>
       
               <input 
                 type="text"
@@ -84,13 +47,16 @@ const Signup = () => {
                 
                 <button type='submit'> Submit</button>
 
-                <div class='signup-signin'>
-                  <p>Sign in here</p>
-                </div>
+               <Link to='/signIn'>
+                  <div class='signup-signin'>
+                      <p>Click to sign in here</p>
+                  </div>
+                </Link> 
       </form>
       
       
     </div>
+    </>
   )
 }
 
