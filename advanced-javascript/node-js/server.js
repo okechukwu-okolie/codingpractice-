f695137ee7da0dc0496c14512d05f7a2ec411cfb
Okolie Samuel <okechukwu.okolie.2023@gmail.com>
2026-02-11 16:26:19 +0100

    aa

--- advanced-javascript/node-js/server.js
+++ advanced-javascript/node-js/server.js
@@ -1,45 +1,45 @@
-// import http from "http";
-// import { locations } from "./db.js";
+import http from "http";
+import { locations } from "./db.js";
 
-// const server = http.createServer(async (req, res) => {
-//   const population = await locations();
+const server = http.createServer(async (req, res) => {
+  const population = await locations();
 
-//   const destination = population
-//     .filter((city) => city.location.toLowerCase().includes("new"))
-//     .map((city) => ({
-//       location: city.location,
-//       facts: city.facts,
-//       population: city.population,
-//     }));
+  const destination = population
+    .filter((city) => city.location.toLowerCase().includes("new"))
+    .map((city) => ({
+      location: city.location,
+      facts: city.facts,
+      population: city.population,
+    }));
 
-//   if (req.url === "/" && req.method === "GET") {
-//     res.setHeader("Content-Type", "application/json");
-//     res.statusCode = 200;
-//     res.end(JSON.stringify(destination));
-//   }
-//    else if (req.url.startsWith("/location/") && req.method === "GET") {
-//     const location = req.url.split("/")[3];
-//     const city = population.filter(
-//       (city) => city.location.toLowerCase() === location.toLowerCase(),
-//     );
-//     res.setHeader("Content-Type", "application/json");
-//     res.statusCode = 200;
-//     res.end(JSON.stringify(city));
-//   }else {
-//     res.statusCode = 404;
-//     res.setHeader("Content-Type", "application/json");
-//     res.end(
-//       JSON.stringify({
-//         error: "notfound",
-//         message: "The requested route does not exist",
-//       }),
-//     );
-//   }
-// });
+  if (req.url === "/" && req.method === "GET") {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
+    res.end(JSON.stringify(destination));
+  }
+   else if (req.url.startsWith("/location/") && req.method === "GET") {
+    const location = req.url.split("/")[3];
+    const city = population.filter(
+      (city) => city.location.toLowerCase() === location.toLowerCase(),
+    );
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
+    res.end(JSON.stringify(city));
+  }else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "application/json");
+    res.end(
+      JSON.stringify({
+        error: "notfound",
+        message: "The requested route does not exist",
+      }),
+    );
+  }
+});
 
-// const PORT = 4000;
-// server.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+const PORT = 4000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 
