4804777d92ce698398f38470ea3a6821c0df8dc3
Okolie Samuel <okechukwu.okolie.2023@gmail.com>
2026-02-16 09:23:47 +0100

    aa

---
+++ advanced-javascript/db2.js
@@ -0,0 +1,49 @@
+import {PGlite} from '@electric-sql/pglite';
+import fs from 'fs';
+//async callback or anonymous function
+//install @electric-sql/pglite using <npm i @electric-sql/pglite>
+(async()=>{
+    //create an instance of PGlite from the constructor
+    const db2 = new PGlite();
+    await db2.exec(`
+        CREATE TABLE IF NOT EXISTS cars(
+        id SERIAL PRIMARY KEY,
+        brand TEXT,
+        model TEXT,
+        year INTEGER,
+        price INTEGER,
+        color TEXT,
+        condition INTEGER,
+        sold BOOLEAN
+    );
+    INSERT INTO cars (brand, model, year, price, color, condition, sold)
+    VALUES
+    ('Toyota', 'Camry', 2020, 25000, 'Blue', 5, false),
+    ('Honda', 'Accord', 2019, 23000, 'Red', 4, true),
+    ('Ford', 'Mustang', 2021, 35000, 'Black', 5, false),
+    ('Chevrolet', 'Malibu', 2018, 20000, 'White', 3, true),
+    ('Tesla', 'Model 3', 2022, 45000, 'Silver', 5, false),
+    ('Toyota', 'Camry', 2015, 15000, 'Green', 4, true),
+    ('Honda', 'Camry', 2016, 18000, 'Yellow', 4, false),
+    ('Ford', 'F-150', 2017, 30000, 'Blue', 5, true),
+    ('Chevrolet', 'Impala', 2019, 22000, 'Red', 3, false),
+    ('Tesla', 'Model S', 2020, 80000, 'Black', 5, true),
+    ('Toyota', 'RAV4', 2021, 28000, 'White', 4, false),
+    ('Honda', 'Camry', 2018, 24000, 'Silver', 4, true),
+    ('Ford', 'Escape', 2016, 19000, 'Green', 3, false),
+    ('Chevrolet', 'Equinox', 2017, 21000, 'Yellow', 4, true),
+    ('Tesla', 'Model X', 2021, 90000, 'Blue', 5, false),
+    ('Toyota', 'Highlander', 2019, 32000, 'Red', 4, true),
+    ('Honda', 'Pilot', 2020, 35000, 'Black', 5, false),
+    ('Ford', 'Explorer', 2018, 28000, 'White', 4, true),
+    ('Chevrolet', 'Traverse', 2019, 26000, 'Silver', 3, false),
+    ('Tesla', 'Model Y', 2022, 50000, 'Green', 5, true);
+    `);
+
+    const query = fs.readFileSync('query.sql', 'utf-8');
+    await db2.exec(query)
+    const response = await db2.query(`SELECT brand, model,year,price FROM cars;`);
+
+    console.clear();
+    console.table(response.rows);
+})();
--- advanced-javascript/query.sql
+++ advanced-javascript/query.sql
@@ -1,5 +1,67 @@
 -- SELECT brand, model, year FROM cars;
 -- SELECT * FROM cars WHERE year > 2015;
-SELECT * FROM cars 
-    WHERE brand NOT LIKE '%Tesla%'
-    -- WHERE model LIKE 'Ca_';
+-- SELECT * FROM cars 
+-- where sold = true or sold = false
+-- -- and (color = 'Red' or color = 'Blue' or color = 'Black')
+-- order by price 
+-- limit 10
+
+-- select * from cars
+-- where price >= 10000 and color = 'Yellow' and sold = true
+
+-- //used for counting
+-- select count(*) from cars
+-- where sold = true
+
+-- used for summing contents in a column
+
+--  here you are referencing just one column for your filtering by using the max key word
+--    select max(price) as most_expensive from cars
+--    where sold =true
+
+-- --    looking for average 
+-- select avg(price) from cars
+-- where model = 'Camry'
+
+-- to round down the zeros(floor and ceiling)
+-- select floor(avg(price)) from cars
+-- where model = 'Camry'
+
+--  here the cal for ceil, avg, etc
+-- select ceil(avg(price)) as avg,
+-- min(price),
+-- max(price) from cars
+-- where sold is true
+
+
+
+-- select model, count(model), floor(avg(price)) as AVG from cars
+-- where sold = false
+-- group by(model)
+-- having count(model) > 0;
+
+
+-- select year,
+-- count(year) as car_count,
+-- max(price),
+--  min(price) 
+-- from cars
+-- where sold = true
+-- group by year
+-- having count(year) > 1 
+-- order by car_count 
+
+
+-- select model, color, year from cars
+-- WHERE sold IS FALSE
+-- -- only show the oldest 5 cars in the database
+-- ORDER BY year
+-- limit 5
+
+
+
+select color , count(color) from cars
+where sold = false 
+group by color
+having count(color) > 1
+order by count(color) desc
